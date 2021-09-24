01efea82887759872411c365196183fa0adf16ba
Christopher Bocardo <ccb012100@users.noreply.github.com>
2021-09-24 11:18:35 -0500

    adds "file" and "context" switches
    - populate ctx_lines from 'context' switch
      - max size of 5, defaults to 0 if not specified
    - read text from input file specified in 'file' switch

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,5 +1,12 @@
 use clap::{App, Arg};
 use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn get_file(file_name: &str) -> File {
+    File::open(file_name).unwrap()
+}
 
 fn main() {
     let args = App::new("grep-lite")
@@ -11,27 +18,54 @@ fn main() {
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("file")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("context")
+                .help("Number of lines of surrounding context to display with matches, max of 5. If not specified, defaults to 0.")
+                .takes_value(true)
+                .required(false),
+        )
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let ctx_lines = 2;
-    let quote = "\
-Every face, every shop,
-bedroom window, public-house, and
-dark square is a picture
-feverishly turned--in search of what?
-It is the same with books.
-What do we seek
-through millions of pages?";
+    let input = args.value_of("file").unwrap();
+    let file = get_file(input);
+    let reader = BufReader::new(file);
+
+    let context_arg = args.value_of("context");
+
+    let ctx_lines: usize = match context_arg {
+        Some(c) => match c.parse::<usize>() {
+            Ok(us) => {
+                if us > 5 {
+                    println!("Invalid 'context' switch value '{}'. Maximum is 5", us);
+                    std::process::exit(1);
+                }
+                us
+            }
+            Err(e) => {
+                println!("Invalid 'context' switch value '{}': {}", c, e);
+                std::process::exit(1);
+            }
+        },
+        // default to 0
+        None => 0,
+    };
 
     let mut tags: Vec<usize> = vec![];
     let mut ctx: Vec<Vec<String>> = vec![];
 
-    for (i, line) in quote.lines().enumerate() {
-        // <3>
-        let contains_substring = re.find(line);
+    for (i, line_) in reader.lines().enumerate() {
+        let line = line_.unwrap();
+        let contains_substring = re.find(&line);
+
         if contains_substring.is_some() {
             tags.push(i);
             ctx.push(Vec::with_capacity(2 * ctx_lines + 1));
@@ -42,17 +76,21 @@ through millions of pages?";
         return;
     }
 
-    for (i, line) in quote.lines().enumerate() {
-        // <6>
+    let file = get_file(input);
+    let reader = BufReader::new(file);
+
+    for (i, line_) in reader.lines().enumerate() {
+        let line = line_.unwrap();
+
         for (j, tag) in tags.iter().enumerate() {
             let lower_bound = tag.saturating_sub(ctx_lines);
             let upper_bound = tag + ctx_lines;
 
             if (i >= lower_bound) && (i <= upper_bound) {
                 if i == *tag {
-                    ctx[j].push(format!("{}:\t{}", i, String::from(line)));
+                    ctx[j].push(format!("{}:\t{}", i, String::from(&line)));
                 } else {
-                    ctx[j].push(format!("{}-\t{}", i, String::from(line)));
+                    ctx[j].push(format!("{}-\t{}", i, String::from(&line)));
                 }
             }
         }
