e638867945e09e0ecfd404c55a8251481514cdc2
Christopher Bocardo <ccb012100@users.noreply.github.com>
2021-09-24 17:57:22 -0500

    add stdin parsing

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,64 +1,28 @@
 use clap::{App, Arg};
 use regex::Regex;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
+use std::io::{prelude::*, BufReader};
+use std::{fs::File, io};
 
 fn get_file(file_name: &str) -> File {
     File::open(file_name).unwrap()
 }
 
-fn main() {
-    let args = App::new("grep-lite")
-        .version("0.1")
-        .about("searches for patterns")
-        .arg(
-            Arg::with_name("pattern")
-                .help("The pattern to search for")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("file")
-                .help("File to search")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("context")
-                .help("Number of lines of surrounding context to display with matches, max of 5. If not specified, defaults to 0.")
-                .takes_value(true)
-                .required(false),
-        )
-        .get_matches();
+fn process_stdin<T: BufRead + Sized>(reader: T, re: Regex) {
+    let mut i = 0;
+    for line_ in reader.lines() {
+        i += 1;
+        let line = line_.unwrap();
+        if re.find(&line).is_some() {
+            println!("{}:\t {}", i, line)
+        }
+    }
+}
 
-    let pattern = args.value_of("pattern").unwrap();
-    let re = Regex::new(pattern).unwrap();
-
-    let input = args.value_of("file").unwrap();
-    let file = get_file(input);
-    let reader = BufReader::new(file);
-
-    let context_arg = args.value_of("context");
-
-    let ctx_lines: usize = match context_arg {
-        Some(c) => match c.parse::<usize>() {
-            Ok(us) => {
-                if us > 5 {
-                    println!("Invalid 'context' switch value '{}'. Maximum is 5", us);
-                    std::process::exit(1);
-                }
-                us
-            }
-            Err(e) => {
-                println!("Invalid 'context' switch value '{}': {}", c, e);
-                std::process::exit(1);
-            }
-        },
-        // default to 0
-        None => 0,
-    };
-
+fn tag_matches<T: BufRead + Sized>(
+    reader: T,
+    re: Regex,
+    ctx_lines: usize,
+) -> (Vec<usize>, Vec<Vec<String>>) {
     let mut tags: Vec<usize> = vec![];
     let mut ctx: Vec<Vec<String>> = vec![];
 
@@ -72,13 +36,15 @@ fn main() {
         }
     }
 
-    if tags.is_empty() {
-        return;
-    }
+    (tags, ctx)
+}
 
-    let file = get_file(input);
-    let reader = BufReader::new(file);
-
+fn process_lines<T: BufRead + Sized>(
+    reader: T,
+    ctx_lines: usize,
+    tags: Vec<usize>,
+    mut ctx: Vec<Vec<String>>,
+) {
     for (i, line_) in reader.lines().enumerate() {
         let line = line_.unwrap();
 
@@ -105,4 +71,73 @@ fn main() {
             println!("{}", x);
         }
     }
+}
+
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("file")
+                .help("File to search")
+                .takes_value(true)
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("context")
+                .help("Number of lines of surrounding context to display with matches, max of 5. If not specified, defaults to 0.")
+                .takes_value(true)
+                .required(false),
+        )
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    let context_arg = args.value_of("context");
+
+    let ctx_lines: usize = match context_arg {
+        Some(c) => match c.parse::<usize>() {
+            Ok(us) => {
+                if us > 5 {
+                    println!("Invalid 'context' switch value '{}'. Maximum is 5", us);
+                    std::process::exit(1);
+                }
+                us
+            }
+            Err(e) => {
+                println!("Invalid 'context' switch value '{}': {}", c, e);
+                std::process::exit(1);
+            }
+        },
+        // default to 0
+        None => 0,
+    };
+
+    let input = args.value_of("file").unwrap_or("-");
+
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_stdin(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        let (tags, ctx) = tag_matches(reader, re, ctx_lines);
+
+        if tags.is_empty() {
+            return;
+        }
+
+        let file = get_file(input);
+        let reader = BufReader::new(file);
+
+        process_lines(reader, ctx_lines, tags, ctx)
+    };
 }
